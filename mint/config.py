6e69c49a7c6541cff8664a5ee78d32cd9fe91107
Alen <alen.kimov@gmail.com>
2024-03-25 23:13:30 +0300

    загрузка сетей из файла

--- mint/config.py
+++ mint/config.py
@@ -1,5 +1,7 @@
 from pydantic import BaseModel
-from twitter.utils import load_toml
+from better_web3.chain import load_chains
+
+from common.utils import load_toml
 
 from common.config import (
     LoggingConfig,
@@ -8,7 +10,7 @@ from common.config import (
     ConcurrencyConfig,
     RequestsConfig,
 )
-from .paths import CONFIG_TOML
+from .paths import CONFIG_TOML, CHAINS_TOML
 
 
 class Config(BaseModel):
@@ -26,3 +28,7 @@ if CONFIG.TWITTER.USE_SUSPENDED_ACCOUNTS:
     VALID_TWITTER_STATUSES.append("SUSPENDED")
 if CONFIG.CAPTCHA.CAPSOLVER_API_KEY:
     VALID_TWITTER_STATUSES.append("LOCKED")
+
+CHAINS, _ = load_chains(load_toml(CHAINS_TOML))
+SEPOLIA_TESTNET   = CHAINS[11155111]
+MINTCHAIN_TESTNET = CHAINS[1686]
